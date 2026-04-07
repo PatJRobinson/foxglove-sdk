3b6fda740831c1d7dd79ae3a41ffedd5212e2f6f
paddy@pj-laptop <patrick.james.robinson@proton.me>
2026-04-07 13:52:30 +0100

    add macos as system

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     ...
   }:
     flake-utils.lib.eachSystem
-    ["x86_64-linux" "aarch64-linux"]
+    ["x86_64-linux" "aarch64-linux" "aarch64-darwin"]
     (
       system: let
         pkgs = import nixpkgs {
